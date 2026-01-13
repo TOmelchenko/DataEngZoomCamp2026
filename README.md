3b6a73e22d531d2136bd4a050f23c4d68b1a6ad7
TOmelchenko <39655695+TOmelchenko@users.noreply.github.com>
2026-01-13 16:42:43 +0000

    Add updates

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # DataEngZoomCamp2026
 Workspace for Data Engineering Zoom Camp 2026
+*.parquet
+
+__pycache_/
+*py[codz]
--- week1/docker-sql/pipeline/pipeline.py
+++ week1/docker-sql/pipeline/pipeline.py
@@ -1,6 +1,6 @@
 import sys
 import pandas as pd
-
+uv install panda
 print("arguments", sys.argv)
 
 day = int(sys.argv[1])
@@ -10,4 +10,4 @@ print(f"Running pipeline for day {day}")
 df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
 print(df.head())
 
-df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")pip install uv
