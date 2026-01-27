bc3a188f19842f1a2ad9a256c38f602046a4ee75
TOmelchenko <39655695+TOmelchenko@users.noreply.github.com>
2026-01-27 20:10:18 +0000

    add terraform files

---
+++ week1/terraform/main.tf
@@ -0,0 +1,69 @@
+terraform {
+  required_version = ">= 1.0"
+  backend "local" {}  # Can change from "local" to "gcs" (for google) or "s3" (for aws), if you would like to preserve your tf-state online
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+    }
+  }
+}
+
+#provider "google" {
+#  project = var.project
+#  region = var.region
+#  credentials = file(var.credentials) 
+#}
+
+provider "google" {
+   project = var.project
+   region  = var.region
+   zone    = var.zone
+ }
+
+# This data source gets a temporary token for the service account
+ data "google_service_account_access_token" "default" {
+   provider               = google
+   target_service_account = "dezc2026-user@dataengineeringzoomcamp2026.iam.gserviceaccount.com"
+   scopes                 = ["https://www.googleapis.com/auth/cloud-platform"]
+   lifetime               = "3600s"
+ }
+ 
+ # This second provider block uses that temporary token and does the real work
+ provider "google" {
+   alias        = "impersonated"
+   access_token = data.google_service_account_access_token.default.access_token
+   project      = var.project
+   region       = var.region
+   zone         = var.zone
+ }
+# Data Lake Bucket
+# Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket
+resource "google_storage_bucket" "data-lake-bucket" {
+  name          = var.bucket_name
+  location      = var.region
+  # Optional, but recommended settings:
+  storage_class = var.storage_class
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled     = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30  // days
+    }
+  }
+
+  force_destroy = true
+}
+
+# DWH
+# Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/bigquery_dataset
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id = var.BQ_DATASET
+  project    = var.project
+} 
---
+++ week1/terraform/variables.tf
@@ -0,0 +1,47 @@
+locals {
+  data_lake_bucket = "dezc2026_data_lake"
+}
+
+
+#variable "credentials" {
+#  description = "My Credentials"
+#  default     = "/Users/tetianaomelchenko/opt/gcp/dataengineeringzoomcamp2026-2f06863ada10.json"
+#  #ex: if you have a directory where this file is called keys with your service account json file
+#  #saved there as my-creds.json you could use default = "./keys/my-creds.json"
+#}
+
+variable "project" {
+  description = "dataengineeringzoomcamp2026"
+}
+
+variable "region" {
+  description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
+  default = "europe-west1"
+  type = string
+}
+
+variable "zone" {
+  default = "europe-west1-b"
+}
+
+variable "bucket_name" {
+  description = "The name of the GCS bucket. Must be globally unique."
+  default = "dezc2026_data_bucket"
+}
+
+variable "storage_class" {
+  description = "Storage class type for your bucket. Check official docs for more info."
+  default = "STANDARD"
+}
+
+variable "BQ_DATASET" {
+  description = "BigQuery Dataset that raw data (from GCS) will be written to"
+  type = string
+  default = "trips_data_all"
+}
+
+variable "TABLE_NAME" {
+  description = "BigQuery Table"
+  type = string
+  default = "ny_trips"
+} 
